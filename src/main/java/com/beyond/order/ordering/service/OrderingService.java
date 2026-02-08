411e362819d1ca957826c32f4e3b3df2855ceeaf
realview45 <kjink0125@gmail.com>
2026-02-09 01:04:05 +0900

    004 db차원에서 메서드의 격리수준을 높이는 어노테이션
    트랜잭션을 논리적으로 직렬실행강제 -> 빈번한 데드락과 에러발생

--- src/main/java/com/beyond/order/ordering/service/OrderingService.java
+++ src/main/java/com/beyond/order/ordering/service/OrderingService.java
@@ -4,19 +4,19 @@ import com.beyond.order.common.service.SseAlarmService;
 import com.beyond.order.member.domain.Member;
 import com.beyond.order.member.repository.MemberRepository;
 import com.beyond.order.ordering.domain.Ordering;
+import com.beyond.order.ordering.domain.OrderingDetails;
 import com.beyond.order.ordering.dtos.OrderingCreateDto;
 import com.beyond.order.ordering.dtos.OrderingListDto;
 import com.beyond.order.ordering.repository.OrderingDetailsRepository;
 import com.beyond.order.ordering.repository.OrderingRepository;
-import com.beyond.order.ordering.domain.OrderingDetails;
-import com.beyond.order.ordering.dtos.OrderingDetailsListDto;
 import com.beyond.order.product.domain.Product;
 import com.beyond.order.product.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +38,7 @@ public class OrderingService {
         this.memberRepository = memberRepository;
         this.sseAlarmService = sseAlarmService;
     }
-
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public Long create(List<OrderingCreateDto> dtoList) {
         //email을 인증객체에서 꺼내서 오더링 빌더로 만들고, 저장,
         String email = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
